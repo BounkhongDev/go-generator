80f03ced131e0ffde0f0cb4b10b42b79099fe448
Bounkhong-AIF <bounkhong-aif@Bounkhong-AIFs-Mac-mini.local>
2026-02-03 11:13:55 +0800

    Add initial project structure with Go generator, including MIT License and updated README for RESTful API scaffolding

--- cmd/generate/main.go
+++ cmd/generate/main.go
@@ -15,52 +15,31 @@ import (
 func main() {
 	flag.Parse()
 	if flag.NArg() < 1 {
-		fmt.Println("Usage: go-gen-r <module_name>||init")
+		fmt.Println("Usage: go-gen-r <module_name>|init")
 		os.Exit(1)
 	}
 
 	moduleName := flag.Arg(0)
-	var projectName string
 
 	if moduleName == "init" {
 		fmt.Print("Enter Project Name: ")
+		var projectName string
 		fmt.Scan(&projectName)
+		projectName = strings.TrimSpace(projectName)
 
-		// validate project name must not empty and must not contain space
 		if projectName == "" {
 			log.Fatal("Project name must not empty")
 		}
-
-		if le := strings.Split(projectName, " "); len(le) > 1 {
+		if strings.Contains(projectName, " ") {
 			log.Fatal("Project name must not contain space")
 		}
-		//validate project name must not -
-		if le := strings.Split(projectName, "-"); len(le) > 1 {
-			log.Fatal("Project name must not contain -")
-		}
 
-		cmd := exec.Command("go", "mod", "init", projectName)
-		if errors.Is(cmd.Err, exec.ErrDot) {
-			cmd.Err = nil
-		}
-		if err := cmd.Run(); err != nil {
-			log.Fatal(err)
-		}
-
-		cmd = exec.Command("go", "mod", "tidy")
-		if errors.Is(cmd.Err, exec.ErrDot) {
-			cmd.Err = nil
-		}
-		if err := cmd.Run(); err != nil {
-			log.Fatal(err)
-		}
-
+		runCmd("go", "mod", "init", projectName)
+		runCmd("go", "mod", "tidy")
 		initPackages()
-
 		generators.GenerateInitialStructure()
 	} else {
-		//To validate module name is have -
-		if le := strings.Split(moduleName, "-"); len(le) > 1 {
+		if strings.Contains(moduleName, "-") {
 			log.Fatal("Module name must not contain -")
 		}
 		generators.GenerateModules(moduleName)
@@ -68,57 +47,25 @@ func main() {
 }
 
 func initPackages() {
-	// get fiber package
-	cmd := exec.Command("go", "get", "github.com/gofiber/fiber/v2")
-	if errors.Is(cmd.Err, exec.ErrDot) {
-		cmd.Err = nil
+	packages := []string{
+		"github.com/gofiber/fiber/v2",
+		"gorm.io/gorm",
+		"github.com/go-playground/validator/v10",
+		"go.uber.org/zap",
+		"github.com/spf13/viper",
+		"gorm.io/driver/postgres",
 	}
-	if err := cmd.Run(); err != nil {
-		log.Fatal(err)
+	for _, pkg := range packages {
+		runCmd("go", "get", pkg)
 	}
+}
 
-	// get gorm package
-	cmd = exec.Command("go", "get", "gorm.io/gorm")
+func runCmd(name string, args ...string) {
+	cmd := exec.Command(name, args...)
 	if errors.Is(cmd.Err, exec.ErrDot) {
 		cmd.Err = nil
 	}
 	if err := cmd.Run(); err != nil {
-		log.Fatal(err)
-	}
-
-	// get validator package
-	cmd = exec.Command("go", "get", "github.com/go-playground/validator/v10")
-	if errors.Is(cmd.Err, exec.ErrDot) {
-		cmd.Err = nil
-	}
-	if err := cmd.Run(); err != nil {
-		log.Fatal(err)
-	}
-
-	// get zaplogger package
-	cmd = exec.Command("go", "get", "go.uber.org/zap")
-	if errors.Is(cmd.Err, exec.ErrDot) {
-		cmd.Err = nil
-	}
-	if err := cmd.Run(); err != nil {
-		log.Fatal(err)
-	}
-
-	// get viper package
-	cmd = exec.Command("go", "get", "github.com/spf13/viper")
-	if errors.Is(cmd.Err, exec.ErrDot) {
-		cmd.Err = nil
-	}
-	if err := cmd.Run(); err != nil {
-		log.Fatal(err)
-	}
-
-	// get postgres gorm driver
-	cmd = exec.Command("go", "get", "gorm.io/driver/postgres")
-	if errors.Is(cmd.Err, exec.ErrDot) {
-		cmd.Err = nil
-	}
-	if err := cmd.Run(); err != nil {
-		log.Fatal(err)
+		log.Fatalf("Command %s %s failed: %v", name, strings.Join(args, " "), err)
 	}
 }
